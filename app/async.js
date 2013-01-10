09b0e4e915d6ddb70f2af6876f4edb3992893839
Nigel Nguyen <nigel@nng.me>
2013-01-10 13:39:40 +1300

    Update app/async.js
    Make it clearer

--- app/async.js
+++ app/async.js
@@ -2,6 +2,7 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 
 define([ 'jquery' ], function($) {
   return {
+    // Can use jQuery helpers for async
     async : function(value) {
 
     },
